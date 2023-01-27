4b7c6e8992c41c05c78a87bc4d01c7e2e7edf5b8
hymetacore <hymetacore@hymetacores-Mac-mini.local>
2023-01-27 00:48:58 -0800

    styling arrow

--- script.js
+++ script.js
@@ -2,27 +2,40 @@ window.onload = function () {
   const prevBtn = document.querySelector(".prevBtn");
   const nextBtn = document.querySelector(".nextBtn");
   let h1 = document.getElementsByTagName("h1")[0];
-  let pageNum = 0;
+  let pageNum = 1;
   let body = document.getElementsByTagName("body")[0];
-  const img = document.querySelectorAll("image");
-  const totalNum = img.length;
+  const img = document.querySelectorAll("img");
+  const section = document.getElementsByTagName("section");
+  const totalNum = section.length;
 
   prevBtn.addEventListener("click", function () {
-    if (pageNum > 0) pageNum--;
-    h1.innerHTML = `now ${pageNum}page`;
-    changeBgImg();
+    if (pageNum > 1) {
+      pageNum--;
+    } else {
+      pageNum = totalNum;
+    }
+    changePage();
   });
 
   nextBtn.addEventListener("click", function () {
-    if (pageNum < totalNum) pageNum++;
-    h1.innerHTML = `now ${pageNum}page`;
-    changeBgImg();
+    if (pageNum < totalNum) {
+      pageNum++;
+    } else {
+      pageNum = 1;
+    }
+    changePage();
   });
 
-  function changeBgImg() {
+  function changePage() {
+    h1.innerHTML = `now ${pageNum}page`;
     body.style.background = img[pageNum];
+    //   window.scrollTo(0, section[pageNum].offsetTop);
+    window.scrollTo({
+      top: section[pageNum - 1].offsetTop,
+      behavior: "smooth",
+    });
   }
 
-  //처음 실행
+  // 처음 실행
   changeBgImg();
 };
